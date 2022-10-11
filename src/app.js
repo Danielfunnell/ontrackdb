fc1ef637033ff8367a95bab3a032544225ab528a
Daniel <danielfunnell@yahoo.co.uk>
2022-10-11 19:38:39 +0100

    npx mix production

--- src/app.js
+++ src/app.js
@@ -3,9 +3,7 @@ require('./js/getMessage');
 require('./js/reply.js');
 require('./js/studentLogin');
 
-
-
-if(window.location.pathname === '/on_track/admin.php'){
+if(window.location.pathname === '/ontrackdb/admin.php'){
   require('./js/getUser.js');
 }
 
